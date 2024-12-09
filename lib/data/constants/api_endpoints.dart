e7dbf02906e8e35349f96e5a85dfbc3ed7b307ec
Dzakwan Irfan <lexdani368@gmail.com>
2024-12-09 11:26:32 +0700

    feat: start from bottom again

--- lib/data/constants/api_endpoints.dart
+++
@@ -1,7 +0,0 @@
-class ApiEndpoints {
-  static const String baseUrl = 'http://localhost/uas-pos';
-  static const String login = '$baseUrl/login';
-  static const String products = '$baseUrl/products';
-  static const String transactions = '$baseUrl/transactions';
-  static const String dashboard = '$baseUrl/dashboard';
-}
